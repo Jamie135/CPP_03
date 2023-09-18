5c9076630272db9b5f28667c4c72045a57410634
unknown <jamie.gworld@hotmail.com>
2023-09-18 22:24:31 +0700

    OK

---
+++ ex02/ClapTrap.hpp
@@ -0,0 +1,24 @@
+#ifndef CLAP_TRAP_HPP
+# define CLAP_TRAP_HPP
+# include <iostream>
+# include <string>
+
+class ClapTrap
+{
+private:
+	std::string		name;
+	unsigned int	hp;
+	unsigned int	energy;
+	unsigned int	attackdmg;
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(ClapTrap &copy);
+	~ClapTrap();
+	ClapTrap &operator=(const ClapTrap &src);
+	void	attack(const std::string &target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+};
+
+#endif
